84f4a7bcf4d06254b9c5c51cd4f6b8d12a99f982
JoaoRafa19 <joaopedrorafael19@gmail.com>
2023-08-08 23:16:03 -0300

    fix: Flex error

--- lib/src/modules/profile/widgets/large/large_profile_page.dart
+++ lib/src/modules/profile/widgets/large/large_profile_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:portifolio/src/modules/profile/readme_header_name.dart';
 import 'package:portifolio/src/modules/profile/widgets/badge.dart';
+import 'package:portifolio/src/modules/profile/widgets/current_projects.dart';
 import 'package:portifolio/src/modules/profile/widgets/large/large_header.dart';
 import 'package:portifolio/src/modules/profile/widgets/large/readme_badge.dart';
 import 'package:portifolio/utils/style.dart';
@@ -19,20 +21,19 @@ class LargeProfilePage extends StatelessWidget {
           children: [
             const LargeHeader(),
             _headerBottom(),
-            // SingleChildScrollView(
-            //   child: Column(
-            //     mainAxisAlignment: MainAxisAlignment.center,
-            //     children: [
-            //       Row(
-            //         mainAxisAlignment: MainAxisAlignment.center,
-            //         children: [
-            //           const Column(), // Profile
-            //           Readme(), // Readme
-            //         ],
-            //       )
-            //     ],
-            //   ),
-            // ),
+            SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Readme(), // Readme
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -80,83 +81,88 @@ class Readme extends StatelessWidget {
   final controller = ScrollController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(
-            Radius.circular(5),
+    return Flex(
+      direction: Axis.vertical,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(5),
+              ),
+              border: Border.all(color: AppStyle.borderGrey)),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const ReadmeHeaderName(),
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Joao Pedro Rafael",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                  color: AppStyle.white,
+                ),
+              ),
+              const Divider(
+                color: Colors.white,
+                height: 25,
+                thickness: 2,
+                indent: 5,
+                endIndent: 5,
+              ),
+              contactRow(),
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Software Developer",
+                style: TextStyle(
+                    color: AppStyle.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+              Image.asset(
+                "images/pato.gif",
+                height: 100,
+                width: 100,
+              ),
+              const Divider(
+                thickness: 10,
+                color: Colors.white,
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.ac_unit,
+                    color: Colors.blue[300],
+                  ),
+                  Text(
+                    "Current Personal Projects:",
+                    style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                          color: AppStyle.white,
+                        ),
+                  ),
+                ],
+              ),
+              const Divider(
+                thickness: 10,
+                color: Colors.white,
+                height: 10,
+              ),
+              const CurrentProjects(projectName: "Backend Go"),
+              const CurrentProjects(projectName: "Websockets Api"),
+              const CurrentProjects(projectName: "Flutter Frontend"),
+              const CurrentProjects(projectName: "React Frontend"),
+            ],
           ),
-          border: Border.all(color: AppStyle.borderGrey)),
-      child: Expanded(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // const ReadmeHeaderName(),
-            // const SizedBox(
-            //   height: 30,
-            // ),
-            // Text(
-            //   "Joao Pedro Rafael",
-            //   style: TextStyle(
-            //     fontWeight: FontWeight.bold,
-            //     fontSize: 40,
-            //     color: AppStyle.white,
-            //   ),
-            // ),
-            // const Divider(
-            //   color: Colors.white,
-            //   height: 25,
-            //   thickness: 2,
-            //   indent: 5,
-            //   endIndent: 5,
-            // ),
-            // contactRow(),
-            // const SizedBox(
-            //   height: 30,
-            // ),
-            // Text(
-            //   "Software Developer",
-            //   style: TextStyle(
-            //       color: AppStyle.white,
-            //       fontSize: 20,
-            //       fontWeight: FontWeight.w600),
-            // ),
-            // Image.asset(
-            //   "images/pato.gif",
-            //   height: 100,
-            //   width: 100,
-            // ),
-            // const Divider(
-            //   thickness: 10,
-            //   color: Colors.white,
-            //   height: 10,
-            // ),
-            // Row(
-            //   children: [
-            //     Icon(
-            //       Icons.ac_unit,
-            //       color: Colors.blue[300],
-            //     ),
-            //     Text(
-            //       "Current Personal Projects:",
-            //       style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-            //             color: AppStyle.white,
-            //           ),
-            //     ),
-            //   ],
-            // ),
-            // const Divider(
-            //   thickness: 10,
-            //   color: Colors.white,
-            //   height: 10,
-            // ),
-            // const CurrentProjects(projectName: "Backend Go"),
-            // const CurrentProjects(projectName: "Websockets Api"),
-            // const CurrentProjects(projectName: "Flutter Frontend"),
-            // const CurrentProjects(projectName: "React Frontend"),
-          ],
         ),
-      ),
+      ],
     );
   }
 
